aff9549ddf30a21718861e9f12a8693a6726f898
peliu <peliu@contractor.lb.com>
2018-02-09 15:36:23 -0500

    Added boostrap to pop up.

---
+++ src/main/java/com/peiwen/tech/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.peiwen.tech.spring5webapp.repositories;
+
+import com.peiwen.tech.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long>{
+}
